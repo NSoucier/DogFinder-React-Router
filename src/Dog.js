1fbdf1cfb1a7b392304926721f4af1600a1ddc5d
Nathan Soucier <soucienp@gmail.com>
2024-06-24 09:11:57 -0400

    updated unwanted route requests

--- src/Dog.js
+++ src/Dog.js
@@ -1,8 +1,15 @@
-import { useParams } from "react-router-dom";
+import { all } from "axios";
+import { useParams, Navigate } from "react-router-dom";
 
 function Dog({ dogs }) {
   const { name } = useParams();
   const dogDetails = dogs.filter((dog) => dog.name === name);
+  const allNames = dogs.map(dog => dog.name);
+
+  if (allNames.indexOf(name) === -1) {
+    return <Navigate to='/dogs'/>
+  }
+
   return (
     <>
       <p>My name is {dogDetails[0].name}.</p>
--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ function Home({ dogs }) {
     return (
         <>
             {dogs.map(dog => (
-                <div>
+                <div key={dog.name}>
                     <p>My name is {dog.name}.</p>
                     <p>I am {dog.age} year(s) old.</p>
                     <p>Fact: {dog.facts[0]}</p>
